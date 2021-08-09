36f8791b9583c0b05f0c2492425fdc2bb2e07c88
Vidya <vzg805@gmail.com>
2021-08-08 23:49:35 -0500

    clusters can be viewed with dropdown!

--- cluster/sketch.js
+++ cluster/sketch.js
@@ -1,3 +1,10 @@
+let sf = 1; // scaleFactor
+let panx = 0; // pan X
+let pany = 0; // pan Y
+let mx, my; // mouse coords;
+let sel;
+var clusterToDisplay = 18;
+
 var index = 0;
 var w = window.innerWidth;
 var h = window.innerHeight;
@@ -16,10 +23,14 @@ function preload() {
 }
 
 function setup() {
+  textAlign(CENTER);
+  sel = createSelect();
+  sel.position(10, 10);
+
   cluster_keys = Object.keys(clusters);
   for (let i = 0; i < cluster_keys.length; i++) {
     const key = cluster_keys[i];
-    // console.log(key, clusters[key]);
+    sel.option(i);
     var oneClusterList = [];
     for (j = 0; j < clusters[key].length; j++) {
       str1 = '../cover-video/images/'
@@ -27,20 +38,56 @@ function setup() {
       oneClusterList.push(loadImage(url));
     }
     allClusterImageList.push(oneClusterList);
+    sel.changed(mySelectEvent);
+
   }
-  console.log(allClusterImageList);
+  // console.log(allClusterImageList);
   createCanvas(w, h);
   background(255);
   frameRate(30);
 }
 
 function draw() {
-  background(255, 255, 255, 20);
+  background(255);
+  mx = mouseX;
+  my = mouseY;
 
-  // index = index + 1;
-  // // if (index == indexMax) {
-  // //   index = index - indexMax;
-  // // }
-  // image(imgList[index], random(w - thumbWidth), random(h - thumbHeight), thumbWidth, thumbHeight);
 
+  translate(mx, my);
+  scale(sf);
+  translate(-mx, -my);
+  translate();
+
+  if (mouseIsPressed) {
+    panx -= pmouseX - mouseX;
+    pany -= pmouseY - mouseY;
+  }
+
+
+  var x = 0;
+  var y = 0;
+  var padding = 20;
+
+  for (let i = 0; i < allClusterImageList[clusterToDisplay].length; i += 1) {
+    image(allClusterImageList[clusterToDisplay][i], x, y, thumbWidth, thumbHeight);
+    if (x > w - thumbWidth - padding) {
+      x = 0
+      y += (thumbHeight + padding)
+    } else {
+      x += (thumbWidth + padding)
+    }
+  }
+
+}
+
+window.addEventListener("wheel", function(e) {
+  if (e.deltaY > 0)
+    sf *= 1.05;
+  else
+    sf *= 0.95;
+});
+
+function mySelectEvent() {
+  clusterToDisplay = parseInt(sel.value());
+  // background(200);
 }
